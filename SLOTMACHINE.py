a2cf1147e08e9b1de5251804834c9699cf42bb9b
Zemo01 <143463198+Zemo01@users.noreply.github.com>
2025-09-01 20:22:44 -0400

    Making sure bet does not exceed balance

--- SLOTMACHINE.py
+++ SLOTMACHINE.py
@@ -50,8 +50,14 @@ def get_bet():
 def main():
     balance = deposit()
     lines = get_number_of_lines()
-    bet = get_bet()
-    total_bet = bet * lines
+    while True:
+        bet = get_bet()
+        total_bet = bet * lines
+
+        if total_bet > balance:
+            print(f"Insufficient balance. Your current balance is: ${balance}")
+        else:
+            break
 
     print(f"You are betting ${bet} on {lines} lines. Total bet is equal to ${total_bet}.")
 
